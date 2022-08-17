293329a3e1ae3b632093911729a47038ac852679
Loi Ngo Van <loivngoo@gmail.com>
2022-08-17 17:51:59 +0700

    modified structure folder

--- backend/user-service_v1/src/main.ts
+++ backend/user-service_v1/src/main.ts
@@ -1,8 +1,16 @@
+import { All } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+    },
+  );
+
+  app.listen();
 }
 bootstrap();
